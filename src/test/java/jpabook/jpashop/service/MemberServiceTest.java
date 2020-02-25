fe8396309833f92513ed8a7bb632fa5bbe5b615e
namseungsik <tmd7031@naver.com>
2020-02-25 23:04:20 +0900

    회원기능 테스트 코드 작성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+//    @Rollback(false)
+//    롤백이면서 쿼리보고싶으면 .flush(); 이용
+    public void 회원가입() throws Exception{
+
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOne(saveId));
+
+    }
+
+    @Test(expected = IllegalStateException.class) // 이렇게하면 try~catch 생략 가능
+    public void 중복_회원_예외() throws Exception{
+
+        // given
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member1.setName("kim1");
+
+        // when
+        memberService.join(member1);
+        memberService.join(member2); // 예외 발생 예정
+
+        // then
+        Assert.fail("예외가 발생해야한다. 여기오면 안됨.");
+    }
+
+}
