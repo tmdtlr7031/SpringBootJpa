9349ad60cf3fabfa4eea9dde378a5f9a5d824d61
namseungsik <tmd7031@naver.com>
2020-03-11 22:38:16 +0900

    주문 기능 테스트

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -16,4 +16,12 @@ public class Address {
     private String street;
     private String zipcode;
 
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    public Address() {
+    }
 }
